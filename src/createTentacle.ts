e948cb101a3d870f3dded12c5c353b7a47ad50eb
何宇 <664475324@qq.com>
2021-11-18 20:44:40 +0800

    0.0.9

--- src/createTentacle.ts
+++ src/createTentacle.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import scheduler from "./scheduler"
 import hooks from "./hooks"
 import { depthCompare, depthClone } from "./utils"
