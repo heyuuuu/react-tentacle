dfae2de6b3794285067e6121490b637f04feb814
何宇 <664475324@qq.com>
2022-01-10 10:40:20 +0800

    0.2.4

--- src/hooks/useReactives.ts
+++ src/hooks/useReactives.ts
@@ -1,30 +1,34 @@
 /// <reference path="../../types/types.d.ts" />
 
-import { useState, useMemo } from "react"
+import { useState, useMemo, useRef } from "react"
 import { compareDeps, depthClone, mixState, replaceObject } from "../utils"
 
 function useReactives<T extends Tentacle.OBJECT>(initState: Partial<T>, deps?: Array<keyof T>) {
 
 	type P = Partial<T>
 
-	const linkState = useMemo(() => initState, [])
+	// 记录当前状态
+	const currentState = useRef<Partial<T>>({})
+
+	const linkState = useMemo(() => {
+		currentState.current = depthClone(initState)
+		return initState
+	}, [])
 
 	const [state, setState] = useState(linkState)
-
-	// 记录当前状态
-	const currentState = useMemo(() => depthClone(state), [state])
 
 	const dispatch = (payload: Tentacle.MixState<P>) => {
 		
 		mixState(state, payload)
 
 		// 检测是否有依赖需要更新
-		const isUpgrade = compareDeps(state, currentState, deps)
+		const isUpgrade = compareDeps(state, currentState.current, deps)
 
 		replaceObject(linkState, state)
 
 		// 如果依赖中存在变动，就触发状态更新
 		if(isUpgrade) {
+			currentState.current = depthClone(state)
 			setState({...state})
 		}
 	}
