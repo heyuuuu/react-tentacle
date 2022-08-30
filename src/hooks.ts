252f62af04ce7afc84b502b92218d0db5980d31a
何宇 <664475324@qq.com>
2022-08-30 18:21:03 +0800

    hooks新增组件轻度状态管理useReactive

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,34 @@
 /// <reference path="../types/types.d.ts" />
 
-import { useMemo, useReducer } from "react"
+import { useMemo, useReducer, useState } from "react"
 import cloneDeep from "lodash.clonedeep"
 import isEqual from "lodash.isequal"
+import createUniqueId from "lodash.uniqueid"
 import immutable from "./immutable"
+
+export function useReactive<T extends Tentacle.OBJECT>(initState: Partial<T>, deps?: (keyof T)[]) {
+    const [uniqueId, setUniqueId] = useState("")
+    const {state, mutation} = useMemo(() => immutable(initState as T), [])
+    const reaction = () => setUniqueId(createUniqueId("reactive-id-"))
+    const reactive = (payload: ((state: T) => Partial<T>) | Partial<T>) => {
+        if(deps) {
+            const snapshot = cloneDeep(state)
+            mutation(payload)
+            const IsUnequal = deps.some(dep => !isEqual(snapshot[dep], state[dep]))
+            IsUnequal && reaction()
+        } else {
+            mutation(payload)
+            reaction()
+        }
+    }
+    return {
+        state,
+        uniqueId,
+        mutation,
+        reaction,
+        reactive,
+    }
+}
 
 export function useReactives<T extends Tentacle.OBJECT>(initState: Partial<T>, deps?: (keyof T)[]) {
     const {state:raw, mutation} = useMemo(() => immutable(initState as T), [])
